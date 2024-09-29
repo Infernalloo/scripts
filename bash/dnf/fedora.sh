eccefb2ad3a996e7df6a670f87a03577f891c040
Infernalloo <infernalloo@proton.me>
2024-09-29 20:16:17 +0200

    changes

--- bash/dnf/fedora.sh
+++ bash/dnf/fedora.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
 echo "Update and upgrade packages"
+sleep 5
 sudo dnf update -y  && sudo dnf upgrade -y
 
 echo "Installing base packages"
+sleep 5
 sudo dnf install git curl wget -y
 
 echo "Installing terminal related packages"
+sleep 5
 sudo dnf install neovim alacritty python  -y
 
 echo "Installing visual studio code"
+sleep 5
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf check-update
 sudo dnf install code -y
 
 echo "Installing Brave"
+sleep 5
 sudo dnf update -y
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 sudo dnf install brave-browser -y
+
+echo "Installing oh-my-bash"
+sleep 5
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+cd
+
+echo "Installing NvChad"
+sleep 5
+git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+cd
--- bash/pacman/pac.sh
+++ bash/pacman/pac.sh
@@ -2,26 +2,41 @@
 
 # Update system
 echo "Updating system"
+sleep 5
 sudo pacman -Syu --noconfirm
 
 # base utilities
 echo "Installing base packages"
+sleep 5
 sudo pacman -S base base-devel git wget curl gpg --noconfirm
 
 # Install yay
 echo "Installing yay"
+sleep 5
 cd
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 cd
 
 # Terminal related
 echo "Installing terminal related packages"
+sleep 5
 sudo pacman -S eza vim neovim alacritty --noconfirm
 sudo pacman -S neofetch bottom cmatrix asciiquarium lolcat --noconfirm
 yay -S bunnyfetch pfetch nitch --noconfirm
 
 # Apps
 echo "Installing applications"
+sleep 5
 yay -S brave --noconfirm
 yay -S visual-studio-code-bin --noconfirm
 yay -S spotify --noconfirm
+
+echo "Installing oh-my-bash"
+sleep 5
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+cd
+
+echo "Installing NvChad"
+sleep 5
+git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+cd
